63468b8ad7b4be4911bb40ebb4ede32a0f39ad57
othmane <othmanekahtal@gmail.com>
2021-12-18 20:38:59 +0100

    handle request and add message errors & handle errors in async functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,24 @@
-const { openConnection } = require("../config/db");
+const {openConnection, passRequest, closeConnection} = require("../config/db");
+const asyncHandler = require('./../utils/asyncHandler')
 exports.getPage = (req, res) => {
-  res.render("login", { title: "user Page", text: "lorem text" });
-  res.end();
+    res.render("login", {error: {}});
+    res.end();
 };
-exports.handleData = async (req, res) => {
-  console.log(req.body);
-  const connection = await openConnection();
-  console.log(connection);
-};
+exports.handleData = asyncHandler(async (req, res) => {
+    const {username, password} = req.body
+    const connection = await openConnection();
+    const sql = `select *
+                 from users
+                 where username = '${username}'
+                   and password = '${password}'`;
+    const {rows} = await passRequest(connection, sql);
+    await closeConnection(connection)
+    if (rows.length) {
+        return res.status(200).render('home')
+    }
+
+    const error = {username: 'username not found !'}
+    const value = {username}
+    res.status(401).render('login', {error, value})
+
+});
--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const {globalHandler} = require('./controllers/globalHandler')
 app.set("view engine", "ejs");
 app.set("views", "./views");
 // middlewares
+/// middleware for handling json requests
 app.use(express.json());
+/// middleware for handling formData
+app.use(express.urlencoded({
+    extended: true
+}))
 app.use(morgan("dev"));
 app.use(express.static("public/"));
 
