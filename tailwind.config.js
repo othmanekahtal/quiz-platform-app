6a223042203d270fd6a7b9c6dd25b6816ac036ad
othmane <othmanekahtal@gmail.com>
2021-12-21 15:41:37 +0100

    add more static resources

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
 module.exports = {
-  purge:["./**/*.{html,ejs,css}"],
-  content: [],
-  darkMode: 'media',
+  content: ["./**/*.{html,ejs}"],
+  darkMode: "media",
   theme: {
     extend: {},
-    fontFamily:{
-      'headline':['Montserrat','sans-serif'],
-      'body':'Roboto Mono, monospace'
-    }
+    fontFamily: {
+      headline: ["Montserrat", "sans-serif"],
+      body: "Roboto Mono, monospace",
+    },
+    gridTemplateColumns: {
+      "dashboard-column-guide": "260px 1fr",
+    },
+    gridTemplateRows: {
+      "dashboard-row-guide": "100px calc(100vh - 100px)",
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
